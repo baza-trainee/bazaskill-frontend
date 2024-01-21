02851233fd8eacf7b64aeb687bce626ca9e377fd
cold <kholodihor@gmail.com>
2024-01-21 10:00:07 +0200

    feat: init project

---
+++ src/app/config/axios.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+
+// instance.interceptors.request.use((config) => {
+//   const token = window.localStorage.getItem('access_token');
+//   config.headers.Authorization = `Bearer ${token}`;
+//   return config;
+// });
+
+export default instance;
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const Home: React.FunctionComponent = () => {
+  return (
+    <div>
+      <h1>Welcome to a baza-work front end web service</h1>
+    </div>
+  );
+};
+
+export default Home;
