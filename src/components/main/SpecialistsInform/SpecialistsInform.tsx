63f01160751c26d27e2abf15675fafe84cd49099
Inna Shkilna <shkinai.1995@gmail.com>
2024-01-26 16:58:42 +0200

    feat:create SpecialistList and SpecialistInform components

---
+++ src/components/main/SpecialistsInform/SpecialistsInform.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const SpecialistsInform = () => {
+  return (
+    <div className="w-[590px] text-white">
+      <h2 className="leading-150 mb-[60px] font-tahoma text-[40px] font-bold  ">
+        Допоможемо знайти гідного спеціаліста
+      </h2>
+      <div className="leading-130  mb-[52px] font-open-sans text-[20px]">
+        <p className="mb-4">
+          Платформа Baza Skill допоможе вам підібрати
+          досвідченого спеціаліста в сфері IT, оскільки
+          кожен з наших кандидатів вже пройшов серйозну
+          практику на Baza Trainee Ukraine.
+        </p>
+
+        <p className="mb-4">
+          Кожен представлений тут фахівець заґартовний в
+          командній роботі, має досвід в розробці проєкту зі
+          зняття вимог, знайомий з труднощами і успіхом на
+          всих життєвих стадіях проєкту.
+        </p>
+
+        <p>
+          Ми віддаємо вам вмілих Junior спеціалістів, які
+          люблять те, чим займаються. Принаймні, так кажуть.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SpecialistsInform;
