5478a069f1102982125ebda9517283dea99e828d
rodindima <rodindima1984@gmail.com>
2024-01-29 10:03:39 +0200

    refactor/rewrite-counters-component

--- src/components/main/counters/Counters.tsx
+++ src/components/main/counters/Counters.tsx
@@ -1,41 +1,81 @@
 'use client';
+import { useState } from 'react';
+
 import CountUp from 'react-countup';
 
-const Counters: React.FC = () => {
-  return (
-    <ul className="container flex grow justify-between gap-1 py-[48px] font-bold text-white">
-      <li className="p-6 text-center ">
-        <p className="text-4xl">
-          <CountUp duration={2.75} start={0} end={39} />
-        </p>
+import VisibilitySensor from 'react-visibility-sensor';
 
-        <p className="text-2xl  ">живих проєктів</p>
-      </li>
-      <li className="p-6 text-center">
-        <p className="text-4xl">
-          <CountUp duration={2.75} start={0} end={350} />
-        </p>
-        <p className="text-2xl  ">залучених учасників</p>
-      </li>
-      <li className="p-6 text-center">
-        <p className="text-4xl">
-          <CountUp duration={2.75} start={0} end={82} />
-        </p>
-        <p className="text-2xl  ">працевлаштованих</p>
-      </li>
-      <li className="p-6 text-center">
-        <p className="text-4xl">
-          <CountUp duration={2.75} start={0} end={12} />
-        </p>
-        <p className="text-2xl  ">технологій</p>
-      </li>
-      <li className="p-6 text-center">
-        <p className="text-4xl">
-          <CountUp duration={2.75} start={0} end={9} />
-        </p>
-        <p className="text-2xl  ">бібліотек</p>
-      </li>
-    </ul>
+
+interface Counters {
+  id: number;
+  count: number;
+  title: string;
+}
+
+const Counters = () => {
+  const [isVisible, setIsVisible] =
+    useState<boolean>(false);
+  const counters: Counters[] = [
+    {
+      id: 1,
+      count: 39,
+      title: 'живих проєктів',
+    },
+    {
+      id: 2,
+      count: 350,
+      title: 'залучених учасників',
+    },
+    {
+      id: 3,
+      count: 82,
+      title: 'працевлаштованих',
+    },
+    {
+      id: 4,
+      count: 12,
+      title: 'технологій',
+    },
+    {
+      id: 5,
+      count: 9,
+      title: 'бібліотек',
+    },
+  ];
+  const handleVisibilityChange = (isVisible: boolean) => {
+    if (isVisible) {
+      setIsVisible(true);
+    }
+  };
+  return (
+    <VisibilitySensor
+      partialVisibility
+      onChange={handleVisibilityChange}
+      offset={{ bottom: 100 }}>
+      {() => (
+        <ul className="container flex grow justify-between gap-1 py-[48px] text-center font-bold text-white">
+          {counters.map((item, index) => {
+            return (
+              <li className="p-6" key={index}>
+                <h3 className="text-[40px] font-bold">
+                  {isVisible ? (
+                    <CountUp
+                      key={item.id}
+                      end={item.count}
+                      duration={2}
+                      redraw={true}
+                    />
+                  ) : (
+                    0
+                  )}
+                </h3>
+                <p className="text-2xl">{item.title}</p>
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </VisibilitySensor>
   );
 };
 
