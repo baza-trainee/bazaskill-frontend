b44ad57333e69844fcfa3665af2af605a2c5ce2f
Inna Shkilna <shkinai.1995@gmail.com>
2024-02-07 20:36:23 +0200

    fix: bag fix

--- src/components/main/modals/forms/register_hr/RegisterHrForm.tsx
+++ src/components/main/modals/forms/register_hr/RegisterHrForm.tsx
@@ -19,7 +19,6 @@ import SelectInput from '@/components/main/ui/form_inputs/SelectInput';
 import TextInput from '@/components/main/ui/form_inputs/TextInput';
 import TextArea from '@/components/main/ui/form_inputs/TextArea';
 import CustomCheckbox from '@/components/main/ui/form_inputs/CustomCheckbox';
-import Select from 'react-select';
 
 const RegisterHrForm = () => {
   const [isProcessing, setIsProcessing] = useState(false);
@@ -50,141 +49,6 @@ const RegisterHrForm = () => {
     }
   };
 
-<<<<<<< HEAD
-  return (
-    <div className="mb-[64px] flex w-full flex-col items-center justify-center">
-      <h1 className="mb-[48px] mt-[60px] text-3xl font-bold">
-        Стати нашим HRom
-      </h1>
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        autoComplete="off"
-        className="mb-[32px] flex flex-col">
-        <div className="flex">
-          <Controller
-            name="first_name"
-            control={control}
-            render={({ field }) => (
-              <TextInput
-                title="Імʼя*"
-                {...field}
-                errorText={errors.first_name?.message}
-                placeholder="Імʼя"
-              />
-            )}
-          />
-          <Controller
-            name="last_name"
-            control={control}
-            render={({ field }) => (
-              <TextInput
-                title="Прізвище*"
-                {...field}
-                errorText={errors.last_name?.message}
-                placeholder="Імʼя"
-              />
-            )}
-          />
-        </div>
-        <div className="flex">
-          <Controller
-            name="phone"
-            control={control}
-            render={({ field }) => (
-              <PhoneInput
-                title="Телефон*"
-                {...field}
-                errorText={errors.phone?.message}
-                placeholder="Телефон"
-              />
-            )}
-          />
-          <Controller
-            name="email"
-            control={control}
-            render={({ field }) => (
-              <TextInput
-                title="Email*"
-                {...field}
-                errorText={errors.email?.message}
-                placeholder="Email"
-              />
-            )}
-          />
-        </div>
-        <div className="flex">
-          <Controller
-            name="company"
-            control={control}
-            defaultValue=""
-            render={({ field }) => (
-              <TextInput
-                title="Компанія"
-                {...field}
-                placeholder="Компанія"
-              />
-            )}
-          />
-          <Controller
-            name="country"
-            control={control}
-            defaultValue=""
-            render={({ field }) => (
-              <SelectInput
-                title="Країна"
-                {...field}
-                options={options}
-                placeholder="Країна"
-              />
-            )}
-          />
-        </div>
-        <div className="flex">
-          <div>
-            <Controller
-              name="specialist"
-              control={control}
-              defaultValue=""
-              render={({ field }) => (
-                <SelectInput
-                  title="Шукаю"
-                  {...field}
-                  options={options}
-                  placeholder="Спеціаліст"
-                />
-              )}
-            />
-            <div className="mt-[32px] flex-col">
-              <CustomCheckbox title="Прошу надіслати договір на ознайомлення" />
-              <CustomCheckbox title="Даю згоду на обробку персональних даних" />
-            </div>
-          </div>
-          <Controller
-            name="message"
-            control={control}
-            render={({ field }) => (
-              <TextArea
-                title="Коментар"
-                {...field}
-                errorText={errors.message?.message}
-                placeholder="Коментар"
-              />
-            )}
-          />
-        </div>
-
-        <div className="">
-          <button
-            type="submit"
-            className="mt-[2rem] w-[231px] rounded-md border border-graphite px-8 py-2 hover:bg-yellow"
-            disabled={
-              errors && !!Object.keys(errors).length
-            }>
-            {isProcessing
-              ? 'Обробка запиту...'
-              : 'Відправити'}
-          </button>
-=======
   const handleClose = () => {
     setIsSubmitted(false); // Скидаємо стан після закриття форми
   };
@@ -283,7 +147,7 @@ const RegisterHrForm = () => {
             <div className="flex">
               <div>
                 <Controller
-                  name="speciality"
+                  name="specialist"
                   control={control}
                   defaultValue=""
                   render={({ field }) => (
@@ -328,7 +192,6 @@ const RegisterHrForm = () => {
               </button>
             </div>
           </form>
->>>>>>> c5d149a0f0457ec2628a22e8936dba7b479cc868
         </div>
       ) : (
         <div>
