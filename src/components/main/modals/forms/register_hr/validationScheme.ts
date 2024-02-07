5d532ab32bb276aa7cbc115829f3e7f42349601b
VladAntonov95 <vladislav.95antonov@gmail.com>
2024-02-07 15:43:16 +0200

    fix(hr-modal): fix validation, add closing message after enter the form

--- src/components/main/modals/forms/register_hr/validationScheme.ts
+++ src/components/main/modals/forms/register_hr/validationScheme.ts
@@ -1,12 +1,31 @@
 import { z } from 'zod';
 
 export const registerScheme = z.object({
-  first_name: z.string(),
+  first_name: z
+    .string()
+    .nonempty('Це поле обовʼязкове')
+    .refine(
+      (value) => /^[a-zA-Zа-яА-Я-їЇ-іІ-ґҐʼ']+$/.test(value),
+      {
+        message:
+          'Тільки букви (без пробілів та спеціальних символів)',
+      }
+    ),
 
-  last_name: z.string(),
+  last_name: z
+    .string()
+    .nonempty('Це поле обовʼязкове')
+    .refine(
+      (value) => /^[a-zA-Zа-яА-Я-їЇ-іІ-ґҐʼ']+$/.test(value),
+      {
+        message:
+          'Тільки букви (без пробілів та спеціальних символів)',
+      }
+    ),
 
   phone: z
     .string()
+    .nonempty('Це поле обовʼязкове')
     .max(
       17,
       'Номер телефону має містити максимум 17 символів'
@@ -19,7 +38,7 @@ export const registerScheme = z.object({
       }
     ),
 
-  email: z.string().email({
+  email: z.string().nonempty('Це поле обовʼязкове').email({
     message: 'Неправильний формат Email',
   }),
 
