480b7db22f21d4d9b8eb5c5100708341637cf2d7
Vladislav Shtrikun <54204831+m0o0n@users.noreply.github.com>
2024-02-09 19:07:53 +0200

    feat(header): responsive layout for header

--- src/components/main/header/Header.tsx
+++ src/components/main/header/Header.tsx
@@ -9,15 +9,18 @@ import { data } from './data';
 
 const Header = () => {
   return (
-    <div className="container relative z-50 flex h-[100px] w-full items-center border-b border-[#4E4E4E] bg-graphite">
-      <div>
-        <Link href="/">
+    <div className="container relative z-50 flex h-[100px] w-full items-center gap-[4px] border-b border-[#4E4E4E] bg-graphite 2xl:gap-[23px] 3xl:gap-[42px] 4xl:gap-[90px] 5xl:gap-[208px]">
+      <div className="w-full xl:h-[40px] xl:w-[160px]">
+        <Link
+          className="flex w-full justify-center "
+          href="/"
+        >
           <Logo />
         </Link>
       </div>
 
-      <div className="ml-[30px] flex grow justify-between gap-[30px]">
-        <div className="flex grow justify-center gap-[15px]">
+      <div className="hidden grow justify-between gap-[4px] xl:flex 2xl:gap-[23px] 3xl:gap-[42px] 4xl:gap-[90px] 5xl:gap-[208px]">
+        <div className="flex grow justify-center gap-0 2xl:gap-[10px] 5xl:gap-[24px]">
           <MenuItem title="Design" inputs={data.design} />
           <MenuItem title="FrontEnd" inputs={data.front} />
           <MenuItem title="BackEnd" inputs={data.full} />
