46ea6ce054d5daf80c206e4c1267663cfe6395e8
Inna Shkilna <shkinai.1995@gmail.com>
2024-02-11 22:17:29 +0200

    refactor: adaptive section help

--- src/components/main/help/Help.tsx
+++ src/components/main/help/Help.tsx
@@ -1,13 +1,36 @@
+'use client';
 import React from 'react';
 import { photosArray } from '@/data/specialists';
 import HelpList from './helpList/HelpList';
 import HelpSpecialist from './helpSpecialist/HelpSpecialist';
+import { useMediaQuery } from '@react-hook/media-query';
 
 const Help = () => {
+  const isExtraLargeScreen = useMediaQuery(
+    '(min-width: 1280px)'
+  );
+  const isLargeScreen = useMediaQuery('(min-width: 768px)');
+  const isMediumScreen = useMediaQuery(
+    '(min-width: 420px)'
+  );
+  const isSmallScreen = useMediaQuery('(min-width: 320px)');
+
+  let numPhotos = 6;
+  if (isExtraLargeScreen) {
+    numPhotos = 16;
+  } else if (isLargeScreen) {
+    numPhotos = 15;
+  } else if (isMediumScreen) {
+    numPhotos = 8;
+  } else if (isSmallScreen) {
+    numPhotos = 6;
+  }
+  const photosToShow = photosArray.slice(0, numPhotos);
+
   return (
-    <section className="container py-[60px]">
-      <div className="flex justify-center gap-[100px] 4xl:gap-[100px] 5xl:gap-[140px]">
-        <HelpList photos={photosArray} />
+    <section className="container  xs:py-[48px] xl:py-[60px]">
+      <div className=" flex xs:flex-col-reverse xs:flex-wrap xs:gap-[24px] md:flex-row md:flex-nowrap md:justify-center md:gap-[40px] xl:gap-[80px] 2xl:gap-[64px] 3xl:gap-[100px]  5xl:gap-[140px]">
+        <HelpList photos={photosToShow} />
         <HelpSpecialist />
       </div>
     </section>
--- src/components/main/help/helpList/HelpList.tsx
+++ src/components/main/help/helpList/HelpList.tsx
@@ -7,8 +7,8 @@ interface HelpListProps {
 
 const HelpList: React.FC<HelpListProps> = ({ photos }) => {
   return (
-    <div className="w-[592px] 5xl:w-[708px]  ">
-      <ul className=" flex  flex-wrap justify-center gap-[32px] 4xl:gap-[40px] 5xl:gap-[50px]">
+    <div className="xs:w-[280px]  sm:w-[380px] md:w-[293px] xl:w-[508px] 2xl:w-[592px] 4xl:w-[604px] 5xl:w-[708px]  ">
+      <ul className=" flex  flex-wrap justify-center xs:gap-[20px] md:gap-[24px] xl:gap-[16px] 2xl:gap-[32px] 4xl:gap-[40px] 5xl:gap-[50px]">
         {photos.map((photo) => (
           <li
             key={photo}
@@ -18,7 +18,7 @@ const HelpList: React.FC<HelpListProps> = ({ photos }) => {
               width={117}
               height={117}
               alt="specialist"
-              className="5xl:h-[132px] 5xl:w-[132px]"
+              className=" xs:h-[80px] xs:w-[80px]  xl:h-[112px] xl:w-[112px] 2xl:h-[117px] 2xl:w-[117px]  5xl:h-[132px] 5xl:w-[132px]"
             />
           </li>
         ))}
