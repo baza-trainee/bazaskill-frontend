4b760ea00d152f14b9f8a1725ffc158f0c40bc75
Vladislav Shtrikun <54204831+m0o0n@users.noreply.github.com>
2024-02-24 19:12:00 +0200

    feat(add main info component): add main info of candidate on the candidate page

--- src/app/[locale]/candidate/[id]/page.tsx
+++ src/app/[locale]/candidate/[id]/page.tsx
@@ -1,10 +1,12 @@
 import CandidateHero from '@/components/candidate/Hero';
+import MainInfo from '@/components/candidate/MainInfo';
 import React from 'react';
 
 const Candidate: React.FC = () => {
   return (
     <div className="bg-graphite">
       <CandidateHero />
+      <MainInfo />
     </div>
   );
 };
