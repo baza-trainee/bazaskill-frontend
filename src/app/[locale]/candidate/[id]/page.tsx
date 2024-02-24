f4389427cfb09be9186a4ccd55a41e673a2fda51
Vladislav Shtrikun <54204831+m0o0n@users.noreply.github.com>
2024-02-24 16:38:04 +0200

    feat(add candidate page): add candidate page and hero section for candidate component

---
+++ src/app/[locale]/candidate/[id]/page.tsx
@@ -0,0 +1,12 @@
+import CandidateHero from '@/components/candidate/Hero';
+import React from 'react';
+
+const Candidate: React.FC = () => {
+  return (
+    <div className="bg-graphite">
+      <CandidateHero />
+    </div>
+  );
+};
+
+export default Candidate;
