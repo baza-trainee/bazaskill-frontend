b7acc452c9d22003b35e750cc01fc07eba86212b
rodindima <rodindima1984@gmail.com>
2024-02-28 15:24:53 +0200

    style(sidebar): add-style-sidebar

--- src/components/admin/sidebar/SideBarItem.tsx
+++ src/components/admin/sidebar/SideBarItem.tsx
@@ -1,8 +1,6 @@
 import ArrowIcon from '@/components/icons/ArrowIcon';
 import Link from 'next/link';
 import React, { useState } from 'react';
-import { usePathname } from 'next/navigation';
-
 interface SideBarItemProps {
   href: string;
   children: React.ReactNode;
