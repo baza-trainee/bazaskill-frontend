44848e8afcefae13f241bb6df6a3599f623ea487
VladAntonov95 <vladislav.95antonov@gmail.com>
2024-03-04 15:05:24 +0200

    feat(admin-settings): add admin settings component, add admin edit settings component

--- src/components/admin/settings/EditSettings.tsx
+++ src/components/admin/settings/EditSettings.tsx
@@ -1,7 +1,139 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
+import Image from 'next/image';
 
 const EditSettings = () => {
-  return <div>EditSettings</div>;
+  const [showOldPassword, setShowOldPassword] =
+    useState(false);
+  const [showNewPassword, setShowNewPassword] =
+    useState(false);
+  const [showRepeatPassword, setShowRepeatPassword] =
+    useState(false);
+
+  const togglePasswordVisibility = (
+    passwordType: string
+  ) => {
+    switch (passwordType) {
+      case 'old':
+        setShowOldPassword((prevState) => !prevState);
+        break;
+      case 'new':
+        setShowNewPassword((prevState) => !prevState);
+        break;
+      case 'repeat':
+        setShowRepeatPassword((prevState) => !prevState);
+        break;
+      default:
+        break;
+    }
+  };
+
+  return (
+    <form
+      className="ml-16 mt-4 w-[596px]"
+      onSubmit={(e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const oldPassword = formData.get('old-password');
+        const newPassword = formData.get('new-password');
+        const repeatPassword = formData.get(
+          'repeat-password'
+        );
+        console.log({
+          oldPassword,
+          newPassword,
+          repeatPassword,
+        });
+      }}
+    >
+      <div className="my-5">
+        <h1 className="mb-5 text-3xl font-bold tracking-wide">
+          Змінити пароль
+        </h1>
+      </div>
+      <div className="mt-10 w-[442px]">
+        <label htmlFor="old-password" className="relative">
+          Старий пароль
+          <input
+            type={showOldPassword ? 'text' : 'password'}
+            id="old-password"
+            name="old-password"
+            placeholder="Старий пароль"
+            required
+            className="mb-10 mt-1 h-[46px] w-full rounded p-2 text-xs text-black"
+          />
+          <Image
+            src="/admin/settings/hide-password.svg"
+            alt="old-password"
+            width={24}
+            height={24}
+            className="absolute bottom-[-4px] left-[405px]"
+            onClick={() => togglePasswordVisibility('old')}
+          ></Image>
+        </label>
+        <label htmlFor="new-password" className="relative">
+          Новий пароль
+          <input
+            type={showNewPassword ? 'text' : 'password'}
+            id="new-password"
+            name="new-password"
+            placeholder="Новий пароль"
+            required
+            className="mb-10 mt-1 h-[46px] w-full rounded p-2 text-xs text-black"
+          />
+          <Image
+            src="/admin/settings/hide-password.svg"
+            alt="new-password"
+            width={24}
+            height={24}
+            className="absolute bottom-[-4px] left-[405px] cursor-pointer"
+            onClick={() => togglePasswordVisibility('new')}
+          ></Image>
+        </label>
+        <label
+          htmlFor="repeat-password"
+          className="relative"
+        >
+          Повторіть новий пароль
+          <input
+            type={showRepeatPassword ? 'text' : 'password'}
+            id="repeat-password"
+            name="repeat-password"
+            placeholder="Повторіть новий пароль"
+            required
+            className="mb-10 mt-1 h-[46px] w-full rounded p-2 text-xs text-black"
+          />
+          <Image
+            src="/admin/settings/hide-password.svg"
+            alt="repeat-password"
+            width={24}
+            height={24}
+            className="absolute bottom-[-4px] left-[405px] cursor-pointer"
+            onClick={() =>
+              togglePasswordVisibility('repeat')
+            }
+          ></Image>
+        </label>
+      </div>
+      <div className="flex">
+        <button
+          type="submit"
+          className="mr-4 h-11 w-[286px] rounded border border-solid border-white hover:bg-white hover:text-black"
+          onClick={() => console.log('відправлення форми')}
+        >
+          Зберігти зміни
+        </button>
+        <button
+          type="reset"
+          className="mr-4 h-11 w-[286px] rounded border border-solid border-white hover:bg-white hover:text-black"
+          onClick={() => console.log('скасування')}
+        >
+          Скасувати
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default EditSettings;
--- src/components/admin/settings/Settings.tsx
+++ src/components/admin/settings/Settings.tsx
@@ -1,7 +1,101 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Settings = () => {
-  return <div>Settings</div>;
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePasswordVisibility = () => {
+    setShowPassword(
+      (prevShowPassword) => !prevShowPassword
+    );
+  };
+
+  return (
+    <form
+      className="ml-16 mt-4 w-[596px]"
+      onSubmit={(e) => {
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget);
+        const email = formData.get('email');
+        const password = formData.get('password');
+        console.log({ email, password });
+      }}
+    >
+      <div className="my-5">
+        <h1 className="mb-5 text-3xl font-bold tracking-wide">
+          Налаштування
+        </h1>
+      </div>
+      <div className="mt-10 w-[442px]">
+        <label htmlFor="email" className="relative">
+          E-mail
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="E-mail"
+            required
+            className="mb-16 mt-1 h-[46px] w-full rounded p-2 text-xs text-black"
+          />
+          <Image
+            src="/admin/settings/pencil.svg"
+            alt="pencil"
+            width={24}
+            height={24}
+            className="absolute bottom-[-4px] left-[405px]"
+          ></Image>
+        </label>
+        <label htmlFor="password" className="relative">
+          Пароль
+          <input
+            type={showPassword ? 'text' : 'password'}
+            id="password"
+            name="password"
+            placeholder="Пароль"
+            required
+            className="mb-10 mt-1 h-[46px] w-full rounded p-2 text-xs text-black"
+          />
+          <Image
+            src="/admin/settings/hide-password.svg"
+            alt="pencil"
+            width={24}
+            height={24}
+            className="absolute bottom-[-4px] left-[405px] cursor-pointer"
+            onClick={togglePasswordVisibility}
+          ></Image>
+          <button type="button">
+            <Link href="settings/edit">
+              <Image
+                src="/admin/settings/pencil.svg"
+                alt="pencil"
+                width={44}
+                height={44}
+                className="absolute bottom-[-13px] left-[465px] bg-white"
+              ></Image>
+            </Link>
+          </button>
+        </label>
+      </div>
+      <div className="flex">
+        <button
+          type="submit"
+          className="mr-4 h-11 w-[286px] rounded border border-solid border-white hover:bg-[#a9a9a9]"
+          onClick={() => console.log('відправлення форми')}
+        >
+          Зберігти зміни
+        </button>
+        <button
+          type="reset"
+          className="mr-4 h-11 w-[286px] rounded border border-solid border-white hover:bg-[#a9a9a9]"
+          onClick={() => console.log('скасування')}
+        >
+          Скасувати
+        </button>
+      </div>
+    </form>
+  );
 };
 
 export default Settings;
