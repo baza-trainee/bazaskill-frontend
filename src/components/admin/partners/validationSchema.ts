e742ab3c2d6fcc9a951afac355686bcae9995d4f
rodindima <rodindima1984@gmail.com>
2024-03-04 14:37:31 +0200

    feat(create-partners-component-admin-panel): add-styles-partners-component-admin-panel

---
+++ src/components/admin/partners/validationSchema.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+export const validationSchema = z.object({
+  name: z
+    .string({
+      required_error: 'Поле повинно бути заповнене',
+    })
+    .min(2, 'Ім’я має містити мінімум 2 символи')
+    .max(25, 'Ім’я має містити максимум 25 символів')
+    .refine(
+      (value) =>
+        /^[a-zA-Zа-яА-ЯҐґЄєІіЇї\s\d$&+,=_@#Nº№'<>^*()%'’`.,:;"()!?/+-]+$/.test(
+          value
+        ),
+      {
+        message: 'Введіть коректне ім’я',
+      }
+    ),
+});
