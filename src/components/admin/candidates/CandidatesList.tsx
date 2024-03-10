ee6bf45d1b36d044d0143019de74401ebe21229c
Vladislav Shtrikun <54204831+m0o0n@users.noreply.github.com>
2024-03-10 18:35:57 +0200

    feat(add plus icon): add plus icon
    n

--- src/components/admin/candidates/CandidatesList.tsx
+++ src/components/admin/candidates/CandidatesList.tsx
@@ -1,10 +1,22 @@
+import PlusIcon from '@/components/icons/Admin-icons/PlusIcon';
+import Link from 'next/link';
 import CandidateCard from './CandidateCard';
 
 const CandidatesList = () => {
   return (
-    <div className=" box-content pr-[24px] 4xl:pr-[196px]">
+    <div className=" box-content pr-[24px] 5xl:pr-[196px]">
       <div className="flex h-fit max-w-[950px] grow flex-wrap items-start justify-start gap-[24px] pl-[24px]">
-        <div className="relative box-border flex h-[486px] w-[442px] max-w-[442px] flex-col gap-[16px] rounded-[10px] border-[2px] border-green bg-transparent px-[40px] py-[32px]"></div>
+        <div className="relative box-border flex h-[486px] w-[442px] items-center justify-center rounded-[10px] border-[2px] border-green bg-transparent">
+          <Link
+            href="/admin/candidates/add"
+            className="flex flex-col items-center gap-[16px] font-sans text-[20px]"
+          >
+            <PlusIcon className="h-[123px] w-[123px] stroke-[#4DC760]" />
+            <p className="ml-[10px] font-sans text-[20px] leading-[1.3] text-[#4DC760]">
+              Додати кандидата
+            </p>
+          </Link>
+        </div>
         <CandidateCard
           specialization="Backend"
           status="searching"
