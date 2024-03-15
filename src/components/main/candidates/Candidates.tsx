7bf24265bdca798d4c71ca272cb25a24a22f9ec8
rodindima <rodindima1984@gmail.com>
2024-03-15 14:58:58 +0200

    fix(section-candidates): add-adaptive-section-candidates

--- src/components/main/candidates/Candidates.tsx
+++ src/components/main/candidates/Candidates.tsx
@@ -3,14 +3,14 @@ import './styles.css';
 
 const Candidates = () => {
   return (
-    <section className="container relative py-[60px]">
-      <h3 className="mb-[50px] text-center font-tahoma text-[24px] font-bold tracking-[1.08px] text-white md:text-4xl">
+    <section className="container relative py-[48px] xl:py-[60px]">
+      <h3 className=" mb-[31px] text-center font-tahoma text-[24px] font-bold tracking-[1.08px] text-white md:text-2xl md:leading-6 lg:mb-[48px] lg:text-4xl xl:mb-[40px] xl:leading-7 2xl:leading-8">
         Наші кандидати
       </h3>
-      <ul className="custom-list grid justify-center gap-4 text-white xs:text-base md:gap-8 md:text-xl xl:grid-cols-2">
+      <ul className="custom-list grid justify-center gap-5 text-white md:grid-cols-2 md:gap-[24px] md:text-base md:leading-7 xl:gap-[21px] xl:text-xl 2xl:gap-[27px]  2xl:leading-7 3xl:gap-[29px] 3xl:leading-6 4xl:gap-[21px]	5xl:gap-[21px]">
         {candidates.map((candidate) => (
           <li
-            className="xs:w-auto md:w-[520px]"
+            className="xs:w-auto xl:max-w-[466px] 2xl:max-w-[503px] 3xl:max-w-[495px] 4xl:max-w-[559px] 5xl:max-w-[654px]"
             key={candidate.id}
           >
             {candidate.text}
