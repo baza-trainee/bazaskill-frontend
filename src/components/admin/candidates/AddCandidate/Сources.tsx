7b6c41ad286d3bc783bd29718c83537f1c545a89
Vladislav Shtrikun <54204831+m0o0n@users.noreply.github.com>
2024-03-29 18:06:59 +0200

    feat(cources component): curces component using field Array

---
+++ src/components/admin/candidates/AddCandidate/Сources.tsx
@@ -0,0 +1,177 @@
+import TrashIcon from '@/components/icons/Admin-icons/TrashIcon';
+import {
+  Control,
+  Controller,
+  DeepMap,
+  FieldError,
+  FieldValues,
+  UseFieldArrayReturn,
+} from 'react-hook-form';
+import FileInput from './FileInput';
+import TextInput from './TextInput';
+
+interface ICourcesProps {
+  control: Control<FieldValues>;
+  fieldArray: UseFieldArrayReturn<
+    FieldValues,
+    'cources',
+    'id'
+  >;
+}
+const Cources: React.FC<ICourcesProps> = ({
+  control,
+  fieldArray: { fields, append, remove },
+}) => {
+  return (
+    <div className="flex w-full flex-col gap-[30px]">
+      {fields.map((field, index) => {
+        return (
+          <div
+            key={field.id}
+            className="flex w-full flex-col gap-[32px]"
+          >
+            <div className="flex w-full gap-[24px]">
+              <Controller
+                name={`cources.${index}.cources_name`}
+                control={control}
+                render={({
+                  field,
+                  formState: { errors },
+                }) => (
+                  <TextInput
+                    {...field}
+                    error={
+                      (
+                        errors.cources as DeepMap<
+                          FieldValues,
+                          FieldError
+                        >
+                      )?.[index]?.name?.message
+                    }
+                    isRequired={true}
+                    placeholder="Ведіть назву"
+                    title="Назва курсів"
+                  />
+                )}
+              />
+              <Controller
+                name={`cources.${index}.cources_specializaton`}
+                control={control}
+                render={({
+                  field,
+                  formState: { errors },
+                }) => (
+                  <TextInput
+                    {...field}
+                    error={
+                      (
+                        errors.cources as DeepMap<
+                          FieldValues,
+                          FieldError
+                        >
+                      )?.[index]?.cources_specializaton
+                        ?.message
+                    }
+                    isRequired={true}
+                    placeholder="Введіть назву"
+                    title="Cпеціальність"
+                  />
+                )}
+              />
+
+              <Controller
+                name={`cources.${index}.cources_sertificate`}
+                control={control}
+                render={({
+                  field: { onChange, value },
+                  formState: { errors },
+                }) => (
+                  <FileInput
+                    onChange={onChange}
+                    value={value}
+                    title="Завантажити сертифікат"
+                    errors={
+                      (
+                        errors.cources as DeepMap<
+                          FieldValues,
+                          FieldError
+                        >
+                      )?.[index]?.cources_sertificate
+                        ?.message
+                    }
+                  />
+                )}
+              />
+            </div>
+
+            <div className="flex w-full gap-[24px]">
+              <Controller
+                name={`cources.${index}.cources_start`}
+                control={control}
+                render={({
+                  field,
+                  formState: { errors },
+                }) => (
+                  <TextInput
+                    {...field}
+                    error={
+                      (
+                        errors.cources as DeepMap<
+                          FieldValues,
+                          FieldError
+                        >
+                      )?.[index]?.cources_start?.message
+                    }
+                    isRequired={true}
+                    placeholder="dd.mm.yyyy"
+                    title="Початок навчання"
+                  />
+                )}
+              />
+
+              <Controller
+                name={`cources.${index}.cources_end`}
+                control={control}
+                render={({
+                  field,
+                  formState: { errors },
+                }) => (
+                  <TextInput
+                    {...field}
+                    error={
+                      (
+                        errors.cources as DeepMap<
+                          FieldValues,
+                          FieldError
+                        >
+                      )?.[index]?.cources_end?.message
+                    }
+                    isRequired={true}
+                    placeholder="dd.mm.yyyy"
+                    title="Випуск"
+                  />
+                )}
+              />
+
+              <div className="flex w-full max-w-[442px] grow flex-col gap-[5px]"></div>
+            </div>
+            <div
+              onClick={() => remove(index)}
+              className="group mx-auto flex h-[44px] w-[120px] cursor-pointer items-center justify-center rounded-[10px] bg-red-600 transition-all duration-300 hover:bg-error"
+            >
+              <TrashIcon className="h-[22px] w-[22px] fill-graphite transition-all duration-300 group-hover:fill-black" />
+            </div>
+          </div>
+        );
+      })}
+      <div
+        onClick={append}
+        className="flex cursor-pointer justify-end"
+      >
+        + Додати ще
+      </div>
+    </div>
+  );
+};
+
+export default Cources;
