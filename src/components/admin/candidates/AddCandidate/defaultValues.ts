410c273ce35e27df9e22a290e78587442353a558
Vladislav Shtrikun <54204831+m0o0n@users.noreply.github.com>
2024-03-29 17:36:24 +0200

    feat(validation and default values): add schema for cv and graduate

--- src/components/admin/candidates/AddCandidate/defaultValues.ts
+++ src/components/admin/candidates/AddCandidate/defaultValues.ts
@@ -1,15 +1,26 @@
 const defaultValues = {
-  name_ua: '',
-  surname_ua: '',
-  name: '',
-  surname: '',
-  country: '',
-  city: '',
-  phone: '',
-  email: '',
-  linkedin: '',
-  discord: '',
-  telegram: '',
+  name_ua: '1',
+  surname_ua: '1',
+  name: '1',
+  surname: '1',
+  country: '1',
+  city: '1',
+  phone: '1',
+  email: 'user@mail.com',
+  linkedin: '1',
+  discord: '1',
+  telegram: '1',
+  cv: null,
+  graduate: [
+    {
+      universiry: '1',
+      universiry_specializaton: '1',
+      universiry_grade: '1',
+      graduate_start: '1',
+      graduate_end: '1',
+      graduate_sertificate: null,
+    },
+  ],
 };
 
 export default defaultValues;
--- src/components/admin/candidates/AddCandidate/schema.ts
+++ src/components/admin/candidates/AddCandidate/schema.ts
@@ -1,4 +1,4 @@
-import { z } from 'zod';
+import { z, ZodType } from 'zod';
 
 const schema = z.object({
   name_ua: z.string().min(1, { message: 'Required' }),
@@ -12,6 +12,43 @@ const schema = z.object({
   linkedin: z.string().min(1, { message: 'Required' }),
   discord: z.string().min(1, { message: 'Required' }),
   telegram: z.string().min(1, { message: 'Required' }),
+  cv: z
+    .any()
+    .refine((value) => value?.length > 0, {
+      message: 'Required',
+    })
+    .refine(
+      (value) => value?.[0]?.size <= 500000,
+      `Max file size is 5MB.`
+    ),
+  graduate: z.array(
+    z.object({
+      universiry: z
+        .string()
+        .min(1, { message: 'Required' }),
+      universiry_specializaton: z
+        .string()
+        .min(1, { message: 'Required' }),
+      universiry_grade: z
+        .string()
+        .min(1, { message: 'Required' }),
+      graduate_start: z
+        .string()
+        .min(1, { message: 'Required' }),
+      graduate_end: z
+        .string()
+        .min(1, { message: 'Required' }),
+      graduate_sertificate: z
+        .any()
+        .refine((value) => value?.length > 0, {
+          message: 'Required',
+        })
+        .refine(
+          (value) => value?.[0]?.size <= 500000,
+          `Max file size is 5MB.`
+        ),
+    })
+  ),
 });
 
 export default schema;
