78aa882453ef75182c9546625fae8f2e1ef2d1b0
Vladislav Shtrikun <54204831+m0o0n@users.noreply.github.com>
2024-03-29 17:38:28 +0200

    feat(graduate component): graduate component with fields array

--- src/components/admin/candidates/AddCandidate/Graduate.tsx
+++ src/components/admin/candidates/AddCandidate/Graduate.tsx
@@ -1,150 +1,185 @@
-import UploadIcon from '@/components/icons/Admin-icons/UploadIcon';
-import { useEffect, useState } from 'react';
-import Dropzone from 'react-dropzone';
-
-const Graduate = () => {
-  const [cv, setCV] = useState<File | null>(null);
-  const [cvError, setCVError] = useState<string>('');
-  const handleUploadCV = async (
-    files: File[] | undefined
-  ) => {
-    if (files && files.length > 0 && files.length === 1) {
-      if (files[0].type === 'application/pdf') {
-        setCV(files[0]);
-        setCVError('');
-      } else {
-        setCV(null);
-        setCVError('Only .pdf format allowed');
-      }
-    } else if (files && files.length > 1) {
-      setCV(null);
-      setCVError('Only one .pdf file allowed');
-    } else {
-      setCV(null);
-      setCVError('An error occured try again');
-    }
-  };
-  const [graduate, setGraduate] = useState<File | null>(
-    null
-  );
-  const [graduateError, setGraduateError] =
-    useState<string>('');
-  useEffect(() => {
-    console.log('here');
-  }, [graduate, setGraduate]);
+import TrashIcon from '@/components/icons/Admin-icons/TrashIcon';
+import {
+  Control,
+  Controller,
+  DeepMap,
+  FieldError,
+  FieldErrors,
+  FieldValues,
+  UseFieldArrayReturn,
+  UseFormRegister,
+} from 'react-hook-form';
+import FileInput from './FileInput';
+import TextInput from './TextInput';
+interface IGraduateProps {
+  control: Control<FieldValues>;
+  errors: FieldErrors<FieldValues>;
+  fieldArray: UseFieldArrayReturn<
+    FieldValues,
+    'graduate',
+    'id'
+  >;
+  register: UseFormRegister<FieldValues>;
+}
+const Graduate = ({
+  control,
+  errors,
+  fieldArray: { fields, append, remove },
+}: IGraduateProps) => {
   return (
-    <div className="flex w-full flex-col">
-      <div className="flex w-full flex-col">
-        <div className="flex w-full gap-[24px]">
-          <div className="flex w-full max-w-[442px] grow flex-col gap-[5px]">
-            <label htmlFor="universiry">
-              Назва навчального закладу{' '}
-              <span className="text-red-500">*</span>
-            </label>
-            <input
-              id="universiry"
-              placeholder="Введіть назву"
-              className="box-border h-[44px] rounded-[4px] px-[16px] py-[6px] text-black outline-none"
-            />
-          </div>
-
-          <div className="flex w-full max-w-[442px] grow flex-col gap-[5px]">
-            <label htmlFor="universiry_specializaton">
-              Cпеціальність{' '}
-              <span className="text-red-500">*</span>
-            </label>
-            <input
-              id="universiry_specializaton"
-              placeholder="Введіть назву"
-              className="box-border h-[44px] rounded-[4px] px-[16px] py-[6px] text-black outline-none"
-            />
-          </div>
-
-          <div className="relative flex w-full max-w-[442px] grow flex-col gap-[5px]">
-            <label htmlFor="graduate">
-              Завантажити CV{' '}
-              <span className="cursor-pointer text-green">
-                [?]{' '}
-              </span>
-              <span className="text-red-500">*</span>
-            </label>
-            <Dropzone
-              onDrop={(acceptedFiles) =>
-                handleUploadCV(acceptedFiles)
-              }
-            >
-              {({ getRootProps, getInputProps }) => (
-                <div
-                  {...getRootProps()}
-                  onClick={(e) => e.stopPropagation()}
-                  className="h-full w-full"
-                >
-                  <input
-                    {...getInputProps()}
-                    id="graduate"
-                    name="graduate"
-                    placeholder="Graduate"
-                    accept=".pdf"
-                    hidden
+    <div className="flex w-full flex-col gap-[30px]">
+      {fields.map((field, index) => {
+        return (
+          <div
+            key={field.id}
+            className="flex w-full flex-col gap-[32px]"
+          >
+            <div className="flex w-full gap-[24px]">
+              <Controller
+                name={`graduate.${index}.universiry`}
+                control={control}
+                render={({ field }) => (
+                  <TextInput
+                    {...field}
+                    error={
+                      (
+                        errors.graduate as DeepMap<
+                          FieldValues,
+                          FieldError
+                        >
+                      )?.[index]?.universiry?.message
+                    }
+                    isRequired={true}
+                    placeholder="Назва навчального закладу"
+                    title="Назва навчального закладу"
                   />
-                  <label
-                    htmlFor="cv"
-                    className="flex h-full w-full cursor-pointer items-center justify-between truncate rounded-[4px] bg-white px-[16px] py-[6px] text-start leading-[26px] text-gray"
-                  >
-                    {cv ? cv.name : 'Завантажте файл'}
-                    <UploadIcon />
-                  </label>
-                </div>
-              )}
-            </Dropzone>
-            <span
-              className={`absolute top-[100%] text-[12px] ${cvError ? 'text-red-500' : 'text-white'}`}
+                )}
+              />
+              <Controller
+                name={`graduate.${index}.universiry_specializaton`}
+                control={control}
+                render={({ field }) => (
+                  <TextInput
+                    {...field}
+                    error={
+                      (
+                        errors.graduate as DeepMap<
+                          FieldValues,
+                          FieldError
+                        >
+                      )?.[index]?.universiry_specializaton
+                        ?.message
+                    }
+                    isRequired={true}
+                    placeholder="Введіть назву"
+                    title="Cпеціальність"
+                  />
+                )}
+              />
+
+              <Controller
+                name={`graduate.${index}.universiry_grade`}
+                control={control}
+                render={({ field }) => (
+                  <TextInput
+                    {...field}
+                    error={
+                      (
+                        errors.graduate as DeepMap<
+                          FieldValues,
+                          FieldError
+                        >
+                      )?.[index]?.universiry_grade?.message
+                    }
+                    isRequired={true}
+                    placeholder="Введіть назву"
+                    title="Cтупінь освіти"
+                  />
+                )}
+              />
+            </div>
+
+            <div className="flex w-full gap-[24px]">
+              <Controller
+                name={`graduate.${index}.graduate_start`}
+                control={control}
+                render={({ field }) => (
+                  <TextInput
+                    {...field}
+                    error={
+                      (
+                        errors.graduate as DeepMap<
+                          FieldValues,
+                          FieldError
+                        >
+                      )?.[index]?.graduate_start?.message
+                    }
+                    isRequired={true}
+                    placeholder="dd.mm.yyyy"
+                    title="Початок навчання"
+                  />
+                )}
+              />
+
+              <Controller
+                name={`graduate.${index}.graduate_end`}
+                control={control}
+                render={({ field }) => (
+                  <TextInput
+                    {...field}
+                    error={
+                      (
+                        errors.graduate as DeepMap<
+                          FieldValues,
+                          FieldError
+                        >
+                      )?.[index]?.graduate_end?.message
+                    }
+                    isRequired={true}
+                    placeholder="dd.mm.yyyy"
+                    title="Випуск"
+                  />
+                )}
+              />
+
+              <Controller
+                name={`graduate.${index}.graduate_sertificate`}
+                control={control}
+                render={({
+                  field: { onChange, value },
+                  formState: { errors },
+                }) => (
+                  <FileInput
+                    onChange={onChange}
+                    value={value}
+                    title="Завантажити сертифікат"
+                    errors={
+                      (
+                        errors.graduate as DeepMap<
+                          FieldValues,
+                          FieldError
+                        >
+                      )?.[index]?.graduate_sertificate
+                        ?.message
+                    }
+                  />
+                )}
+              />
+            </div>
+            <div
+              onClick={() => remove(index)}
+              className="group mx-auto flex h-[44px] w-[120px] cursor-pointer items-center justify-center rounded-[10px] bg-red-600 transition-all duration-300 hover:bg-error"
             >
-              {cvError
-                ? cvError
-                : 'Baza Skill приймає резюме в форматі PDF, розміром до 5 Mb'}
-            </span>
+              <TrashIcon className="h-[22px] w-[22px] fill-graphite transition-all duration-300 group-hover:fill-black" />
+            </div>
           </div>
-        </div>
-
-        <div className="flex w-full gap-[24px]">
-          <div className="flex max-w-[442px] grow flex-col gap-[5px]">
-            <label htmlFor="name_ua">
-              Ім`я <span className="text-red-500">*</span>
-            </label>
-            <input
-              id="name_ua"
-              name="name_ua"
-              placeholder="Ім`я"
-              className="box-border h-[44px] rounded-[4px] px-[16px] py-[6px] text-black outline-none"
-            />
-          </div>
-
-          <div className="flex max-w-[442px] grow flex-col gap-[5px]">
-            <label htmlFor="surname_ua">
-              Прізвище{' '}
-              <span className="text-red-500">*</span>
-            </label>
-            <input
-              id="surname_ua"
-              name="surname_ua"
-              placeholder="Прізвище"
-              className="box-border h-[44px] rounded-[4px] px-[16px] py-[6px] text-black outline-none"
-            />
-          </div>
-
-          <div className="flex max-w-[442px] grow flex-col gap-[5px]">
-            <label htmlFor="country">
-              Країна <span className="text-red-500">*</span>
-            </label>
-            <input
-              id="country"
-              name="country"
-              placeholder="Країна"
-              className="box-border h-[44px] rounded-[4px] px-[16px] py-[6px] text-black outline-none"
-            />
-          </div>
-        </div>
+        );
+      })}
+      <div
+        onClick={append}
+        className="flex cursor-pointer justify-end"
+      >
+        + Додати ще
       </div>
     </div>
   );
