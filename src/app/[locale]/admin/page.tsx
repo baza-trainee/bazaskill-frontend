bd34ad9eb5cd9c584cc62e7f2dd23321719e6451
cold <kholodihor@gmail.com>
2024-04-17 08:54:19 +0300

    fix(auth): fix auth layout

--- src/app/[locale]/admin/page.tsx
+++ src/app/[locale]/admin/page.tsx
@@ -1,17 +1,7 @@
 'use client';
-import { useLayoutEffect } from 'react';
-import { redirect } from 'next/navigation';
-import Loader from '@/components/admin/ui/Loader';
 
 const AdminPage: React.FC = () => {
-  useLayoutEffect(() => {
-    const token = localStorage.getItem('token');
-    if (!token) {
-      redirect('/login');
-    } else redirect('/admin/candidates');
-  }, []);
-
-  return <Loader />;
+  return <div>Main admin page</div>;
 };
 
 export default AdminPage;
