155be8a672e6a521b8a0e9db8efde870f7ebddd2
JuliaTarasenko00 <118853187+JuliaTarasenko00@users.noreply.github.com>
2024-05-22 12:51:56 +0300

    fix testimonals section

--- src/components/main/testimonials/Testimonials.tsx
+++ src/components/main/testimonials/Testimonials.tsx
@@ -1,20 +1,10 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-import { RefObject, useCallback, useRef } from 'react';
-import {
-  Swiper,
-  SwiperRef,
-  SwiperSlide,
-} from 'swiper/react';
 import { useTranslations } from 'next-intl';
-import { Navigation, Pagination } from 'swiper/modules';
-import { Testimonial } from '@/types/testimonials';
 import { useQuery } from '@tanstack/react-query';
 import { constants } from '@/constants';
 import { getTestimonials } from '@/api/testimonials';
-import ButtonRight from '@/components/icons/ButtonRight';
-import ButtonLeft from '@/components/icons/ButtonLeft';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
@@ -22,7 +12,7 @@ import 'swiper/css/navigation';
 import './testimonials_styles.css';
 
 const DynamicCard = dynamic(
-  () => import('./TestimonialCard'),
+  () => import('./TestimonialSlider'),
   {
     loading: () => null,
   }
@@ -30,24 +20,11 @@ const DynamicCard = dynamic(
 
 const Testimonials = () => {
   const t = useTranslations('Main.testimonials');
-  const sliderRef: RefObject<SwiperRef> = useRef(null);
 
   const { data } = useQuery({
     queryKey: [constants.testimonials.FETCH_TESTIMONIALS],
     queryFn: getTestimonials,
   });
-
-  const handlePrev = useCallback(() => {
-    if (!sliderRef.current) return;
-
-    sliderRef.current.swiper.slidePrev();
-  }, []);
-
-  const handleNext = useCallback(() => {
-    if (!sliderRef.current) return;
-
-    sliderRef.current.swiper.slideNext();
-  }, []);
 
   return (
     <section className="container relative py-[60px]">
@@ -55,47 +32,9 @@ const Testimonials = () => {
         {t('title')}
       </h3>
       <div className="relative xl:mx-auto xl:max-w-[1280px]  5xl:max-w-[1681px]">
-        <Swiper
-          key={'testimonials'}
-          slidesPerView={'auto'}
-          spaceBetween={34}
-          loop={true}
-          pagination={{
-            clickable: true,
-          }}
-          ref={sliderRef}
-          navigation={{
-            prevEl: '.prev-testimonials',
-            nextEl: '.next-testimonials',
-          }}
-          modules={[Pagination, Navigation]}
-          className="testimonials max-w-[88%] 5xl:max-w-[1524px]"
-        >
-          {data &&
-            Array.isArray(data) &&
-            data?.map((item: Testimonial) => (
-              <SwiperSlide
-                className="testimonials_slide max-w-[100%] 5xl:max-w-[745px]"
-                key={item.id}
-              >
-                <DynamicCard item={item} />
-              </SwiperSlide>
-            ))}
-        </Swiper>
-        <button
-          type="button"
-          className="prev absolute left-0 top-[24%] z-20 hidden translate-y-[-24%] cursor-pointer md:block"
-          onClick={handlePrev}
-        >
-          <ButtonLeft className="fill-white" />
-        </button>
-        <button
-          type="button"
-          className="next absolute right-0 top-[24%] z-20 hidden translate-y-[-24%] cursor-pointer md:block"
-          onClick={handleNext}
-        >
-          <ButtonRight className="fill-white" />
-        </button>
+        {data && Array.isArray(data) && (
+          <DynamicCard data={data} />
+        )}
       </div>
     </section>
   );
