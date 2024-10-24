fbe79a7992a77df98942a9376189989856926010
JuliaTarasenko00 <yuliiatarasenko.work@gmail.com>
2024-10-24 16:30:41 +0300

    added section our history

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -6,6 +6,7 @@ import Counters from '@/components/user/counters/Counters';
 import Help from '@/components/user/help/Help';
 import Hero from '@/components/user/hero/Hero';
 import CookiesModal from '@/components/user/modals/cookies/CookiesModal';
+import { OurHistory } from '@/components/user/our_history/OurHistory';
 import Partners from '@/components/user/partners/Partners';
 import Posts from '@/components/user/posts/Posts';
 import Testimonials from '@/components/user/testimonials/Testimonials';
@@ -14,6 +15,7 @@ async function Home() {
   return (
     <div className="mt-[80px] md:mt-[100px] flex min-h-screen w-full flex-col items-center justify-center bg-graphite">
       <Hero />
+      <OurHistory/>
       <Counters />
       {/* <Help /> */}
       {/* <ContentCards /> */}
