4d95612162d9568130fd7400f796b33a5a572d54
cold <kholodihor@gmail.com>
2024-10-29 09:59:55 +0200

    feat: add section s to "why juniors" page

--- src/components/user/help/Help.tsx
+++ src/components/user/help/Help.tsx
@@ -46,7 +46,10 @@ function Help() {
       className="aboutus relative xs:py-[48px] xl:py-[60px]"
       id="help"
     >
-      <div className="flex xs:flex-col-reverse xs:flex-wrap xs:gap-[24px] md:flex-row md:flex-nowrap md:justify-center md:gap-[40px] xl:gap-[80px] 2xl:gap-[64px] 3xl:gap-[100px]  5xl:gap-[140px]">
+      <div className="flex xs:flex-col-reverse xs:flex-wrap xs:gap-[24px] 
+      md:flex-row md:flex-nowrap md:justify-center md:gap-[40px] 
+      xl:gap-[80px] 2xl:gap-[64px] 3xl:gap-[100px]  5xl:gap-[140px]">
+        <HelpSpecialist />
         {data && data.length
           ? (
               <HelpList photos={data!.slice(0, numPhotos)} />
@@ -54,7 +57,6 @@ function Help() {
           : (
               <HelpListSkeleton />
             )}
-        <HelpSpecialist />
       </div>
     </section>
   );
---
+++ src/components/user/juniors-advantages/JuniorsAdvantages.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const JuniorsAdvantages = () => {
+  return (
+    <div className='min-h-screen w-full text-white flex flex-col justify-center items-center'>
+      JuniorsAdvantages 
+    </div>
+  )
+}
+
+export default JuniorsAdvantages
