1a49a55fcbd4c6eb54b1ebbfcc916ecae61c6719
cold <kholodihor@gmail.com>
2024-10-30 12:10:45 +0200

    feat: add junior`s advantages section

--- src/app/[locale]/(user)/why-juniors/page.tsx
+++ src/app/[locale]/(user)/why-juniors/page.tsx
@@ -20,7 +20,7 @@ export async function generateMetadata({ params }: PageProps): Promise<Metadata>
 
 export default function WhyJuniorsPage(): JSX.Element {
   return ( 
-  <div className='pt-[80px] md:pt-[150px]'> 
+  <div className='pt-[80px] md:pt-[150px] bg-graphite'> 
     <Help />
     <ContentCards />
     <JuniorsAdvantages/>
