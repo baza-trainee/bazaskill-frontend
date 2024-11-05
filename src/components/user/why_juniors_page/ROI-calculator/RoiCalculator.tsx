3314ae71cdcff5a093eaf2e4ae509bac260d9f13
Brodich Vlad <brodich_vlad@ukr.net>
2024-11-05 13:45:30 +0200

    feat: adaptive RoiCalculator

--- src/components/user/why_juniors_page/ROI-calculator/RoiCalculator.tsx
+++ src/components/user/why_juniors_page/ROI-calculator/RoiCalculator.tsx
@@ -1,14 +1,17 @@
+import { useTranslations } from "next-intl";
 
 export default function RoiCalculator(): JSX.Element {
+  const t = useTranslations('Why_juniors.roi_calculator');
+
   return (
-    <section className="md:main-texture-background bg-no-repeat bg-cover bg-fixed text-white py-[100px]">
+    <section className="md:main-texture-background bg-no-repeat bg-cover bg-fixed text-white py-12 md:py-[60px] xl:py-[100px]">
 
-      <div className="container mx-auto flex items-center justify-between gap-8">
+      <div className="container mx-auto flex flex-col md:flex-row md:items-center md:justify-between gap-8">
 
-        <h2 className="text-xl md:text-[40px] leading-normal font-tahoma font-bold">Спробуй калькулятор рентабельності інвестицій junior&middle</h2>
+        <h2 className="sm:max-w-[460px] md:max-w-[600px] xl:max-w-[890px] text-xl md:text-2xl xl:text-4xl font-tahoma font-bold ">{t("title")}</h2>
 
         <button type="button" 
-          className="min-w-[230px] sm:max-w-[280px] flex items-center justify-center px-1 py-4 main-gradient text-black rounded-md  text-xl font-semibold">ROI калькулятор</button>
+          className="sm:mx-auto md:mx-0 min-w-[230px] sm:max-w-[280px] flex items-center justify-center px-1 py-4 main-gradient text-black rounded-md text-xl font-semibold">{t("roi_btn")}</button>
       </div>
     </section>
   )
